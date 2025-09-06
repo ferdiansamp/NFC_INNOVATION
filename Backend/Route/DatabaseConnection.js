90a9dc77bfb8ef1e058f77c0b74a73bf97ce216d
HAWKCAN <farizrahman335577@gmail.com>
2025-09-06 22:08:42 +0700

    p

--- Backend/Route/DatabaseConnection.js
+++ Backend/Route/DatabaseConnection.js
@@ -6,11 +6,11 @@ dotenv.config();
 // Kalau ada env dari Railway → pakai itu
 // Kalau tidak ada (di lokal) → pakai env manual dari .env
 const DB = mysql.createPool({
-  host: process.env.MYSQLHOST || process.env.DB_HOST || "127.0.0.1",
-  user: process.env.MYSQLUSER || process.env.DB_USER || "root",
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || "",
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME || "test",
-  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  host:  process.env.DB_HOST || "127.0.0.1",
+  user:process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database:  process.env.DB_NAME || "test",
+  port:  process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
