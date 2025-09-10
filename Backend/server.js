5288774e74eb647df1c5174fa831c51b135ff288
HAWKCAN <farizrahman335577@gmail.com>
2025-09-10 13:12:02 +0700

    testing

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/penumpang", penumpangRoutes);
-app.use("/api", pelangganRoutes);
+app.use("/api/pelanggan", pelangganRoutes);
 app.use("/api/tiket", Tiket);
 
 
